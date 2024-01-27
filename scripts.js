4a7ab03e626e65c9ddf39fdfed6abc3abe00dbf3
Franco <franconicolasvidal@gmail.com>
2024-01-27 00:23:33 -0300

    Dimensiones justadas en mobile

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    function eliminarElementoEnPantallaPequena() {
+        var elemento = document.getElementById('telefono');
+        if (elemento && window.innerWidth <= 767) {
+          elemento.parentNode.removeChild(elemento);
+        }
+      }
+    
+      // Llamada inicial para comprobar el tamaño de la pantalla
+      eliminarElementoEnPantallaPequena();
+    
+      // Añade un listener para volver a comprobar cuando se redimensiona la ventana
+      window.addEventListener('resize', eliminarElementoEnPantallaPequena);
 });
