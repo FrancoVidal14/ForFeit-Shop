f16257fbdb3dbc467ecb8dd1deb16e90f339f18f
Franco <franconicolasvidal@gmail.com>
2024-01-28 15:52:53 -0300

    Slider caracteristicas en mobile

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  //////////////////////////////////
+  //Funciones del Header y Navbar//
+  ////////////////////////////////
   function ocultarElementosEnPantallaPequena() {
     let elementoTelefono = document.getElementById('telefono');
     if (elementoTelefono) {
@@ -23,44 +25,74 @@ document.addEventListener("DOMContentLoaded", function() {
       contenedorOriginal.appendChild(navbar);
     }
   }
+    // Llama a la función inicialmente y escucha cambios de tamaño de la ventana
+    ajustarNavbar();
+    window.addEventListener('resize', ajustarNavbar);
+  
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+  
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+  
+    document.querySelectorAll(".nav-link").forEach(link => {
+      link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+      });
+    });
+  
+  //////////////////////////////
+  //Funciones caracteristicas//
+  ////////////////////////////
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.querySelector('.slider-container');
+    const totalSlides = document.querySelectorAll('.slider-content').length;
+    let currentSlide = 0;
+
+    function showSlide(index) {
+      currentSlide = index;
+      slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      showSlide(currentSlide);
+    }
+
+    // Controlador de desplazamiento automático cada 5 segundos
+    setInterval(nextSlide, 5000);
+
+    // Inicia el slider al cargar la página
+    showSlide(currentSlide);
+  });
+
+  /////////////////////////////
+  //Funciones del Bento Grid//
+  ///////////////////////////
 
   function ajustarBotonesEnPantallaPequena() {
     const botonesCompra = document.querySelectorAll('.hiddenbtn');
-  
+      
     if (window.innerWidth <= 767) {
-      botonesCompra.forEach((boton) => {
+        botonesCompra.forEach((boton) => {
         boton.style.display = 'none';
       });
     } else {
-      botonesCompra.forEach((boton) => {
+        botonesCompra.forEach((boton) => {
         boton.style.display = 'block';
       });
     }
-  }
+    }
+      
+      // Llamada inicial para comprobar el tamaño de la pantalla
+      ajustarBotonesEnPantallaPequena();
+      
+      // Añade un listener para volver a comprobar cuando se redimensiona la ventana
+      window.addEventListener('resize', ajustarBotonesEnPantallaPequena);
+    
   
-  // Llamada inicial para comprobar el tamaño de la pantalla
-  ajustarBotonesEnPantallaPequena();
-  
-  // Añade un listener para volver a comprobar cuando se redimensiona la ventana
-  window.addEventListener('resize', ajustarBotonesEnPantallaPequena);
-
-  // Llama a la función inicialmente y escucha cambios de tamaño de la ventana
-  ajustarNavbar();
-  window.addEventListener('resize', ajustarNavbar);
-
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
-
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  });
-
-  document.querySelectorAll(".nav-link").forEach(link => {
-    link.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-    });
-  });
-
 });
