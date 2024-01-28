f6756b99835bb672e56d3b43771738f534c10a94
Franco <franconicolasvidal@gmail.com>
2024-01-27 22:25:07 -0300

    Terminado Header Inicio y Bento en pc y mobile

--- scripts.js
+++ scripts.js
@@ -1,28 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  function ocultarElementoEnPantallaPequena() {
-    var elementoTelefono = document.getElementById('telefono');
+  function ocultarElementosEnPantallaPequena() {
+    let elementoTelefono = document.getElementById('telefono');
     if (elementoTelefono) {
       elementoTelefono.style.display = (window.innerWidth <= 767) ? 'none' : '';
-    }
   }
+}
 
-  ocultarElementoEnPantallaPequena();
-  window.addEventListener('resize', ocultarElementoEnPantallaPequena);
+  ocultarElementosEnPantallaPequena();
+  window.addEventListener('resize', ocultarElementosEnPantallaPequena);
 
   function ajustarNavbar() {
-    var contenedorMovil = document.querySelector('.first-sec-head');
-    var navbar = document.querySelector('.navbar');
+    let contenedorMovil = document.querySelector('.first-sec-head');
+    let navbar = document.querySelector('.navbar');
 
     // Mueve el navbar al contenedor en dispositivos móviles
     if (window.innerWidth <= 767) {
       contenedorMovil.appendChild(navbar);
     } else {
       // Restaura el navbar a la segunda sección si no es un dispositivo móvil
-      var contenedorOriginal = document.querySelector('.second-sec-head');
+      let contenedorOriginal = document.querySelector('.second-sec-head');
       contenedorOriginal.appendChild(navbar);
     }
   }
+
+  function ajustarBotonesEnPantallaPequena() {
+    const botonesCompra = document.querySelectorAll('.hiddenbtn');
+  
+    if (window.innerWidth <= 767) {
+      botonesCompra.forEach((boton) => {
+        boton.style.display = 'none';
+      });
+    } else {
+      botonesCompra.forEach((boton) => {
+        boton.style.display = 'block';
+      });
+    }
+  }
+  
+  // Llamada inicial para comprobar el tamaño de la pantalla
+  ajustarBotonesEnPantallaPequena();
+  
+  // Añade un listener para volver a comprobar cuando se redimensiona la ventana
+  window.addEventListener('resize', ajustarBotonesEnPantallaPequena);
 
   // Llama a la función inicialmente y escucha cambios de tamaño de la ventana
   ajustarNavbar();
